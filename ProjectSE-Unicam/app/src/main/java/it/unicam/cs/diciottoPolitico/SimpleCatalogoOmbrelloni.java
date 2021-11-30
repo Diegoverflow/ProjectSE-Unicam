b0466054d0400ef3e4ce2dacf73a039a6f850b20
Piermuz <piermichele.rosati@gmail.com>
2021-11-30 13:02:25 +0100

    Implementati tutti i metodi

--- ProjectSE-Unicam/app/src/main/java/it/unicam/cs/diciottoPolitico/SimpleCatalogoOmbrelloni.java
+++ ProjectSE-Unicam/app/src/main/java/it/unicam/cs/diciottoPolitico/SimpleCatalogoOmbrelloni.java
@@ -2,9 +2,10 @@ package it.unicam.cs.diciottoPolitico;
 
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
- * Semplice implementazione di un catalogo ombrelloni.
+ * Implementazione di una semplice classe per il catalogo ombrelloni.
  */
 public class SimpleCatalogoOmbrelloni implements CatalogoOmbrelloni {
 
@@ -25,19 +26,19 @@ public class SimpleCatalogoOmbrelloni implements CatalogoOmbrelloni {
 
     @Override
     public boolean removeRigaOmbrellone(Ombrellone ombrellone) {
-        if (ombrellone == null)
-            throw new NullPointerException("Ombrellone null!");
+        this.ombrelloneNullException(ombrellone);
         return this.righe.removeIf(rigaCatalogo ->
                 rigaCatalogo.getOmbrellone().getId() == ombrellone.getId());
     }
 
     @Override
     public boolean modificaPrezzo(Ombrellone ombrellone, double nuovoPrezzo) {
-        if (ombrellone == null)
-            throw new NullPointerException("Ombrellone null!");
+        this.ombrelloneNullException(ombrellone);
         if (this.getRigaCatalogoBy(ombrellone).isPresent())
             return false;
         RigaCatalogo rigaCatalogo = this.getRigaCatalogoBy(ombrellone).get();
+        if (rigaCatalogo.getPrezzoOmbrellone() == nuovoPrezzo)
+            return false;
         this.righe.remove(rigaCatalogo);
         rigaCatalogo.setPrezzoOmbrellone(nuovoPrezzo);
         return this.righe.add(rigaCatalogo);
@@ -51,11 +52,15 @@ public class SimpleCatalogoOmbrelloni implements CatalogoOmbrelloni {
                 this.righe.stream()
                         .filter(r -> r.getOmbrellone().getId() == id)
                         .findFirst();
+        if (rigaCatalogo.isEmpty())
+            return Optional.empty();
         return Optional.of(rigaCatalogo.get().getOmbrellone());
     }
 
     @Override
     public List<Ombrellone> getOmbrelloniBy(double prezzo) {
+        if (prezzo < 0)
+            throw new IllegalArgumentException("Prezzo non valido!");
         List<RigaCatalogo> righeCatalogo = this.righe.stream()
                 .filter(r -> r.getPrezzoOmbrellone() == prezzo)
                 .collect(Collectors.toList());
@@ -67,14 +72,33 @@ public class SimpleCatalogoOmbrelloni implements CatalogoOmbrelloni {
 
     @Override
     public List<Ombrellone> getOmbrelloniBy(Date data, FasciaOraria fasciaOraria) {
-        // TODO: implementare
-        return null;
+        if (data == null)
+            throw new NullPointerException("Data null!");
+        if (fasciaOraria == null)
+            throw new NullPointerException("Fascia oraria null!");
+        // TODO: Inserire controlli eccezione per data e fascia oraria non validi, ovvero se sono precedenti al giorno attuale
+        // TODO: da testare!
+        List<RigaCatalogo> rigaCatalogoStream = this.righe.stream()
+                .filter(r -> r.getPrenotazioni()
+                        .stream()
+                        .filter(p -> p.getDataPrenotazione().equals(data)
+                                && p.getFasciaOraria().equals(fasciaOraria)).isParallel()).toList();
+        List<Ombrellone> ombrelloni = new ArrayList<>();
+        for (RigaCatalogo r : rigaCatalogoStream)
+            ombrelloni.add(r.getOmbrellone());
+        return ombrelloni;
     }
 
     @Override
     public Optional<RigaCatalogo> getRigaCatalogoBy(Ombrellone ombrellone) {
+        this.ombrelloneNullException(ombrellone);
         return this.righe.stream()
                 .filter(r -> r.getOmbrellone().equals(ombrellone))
                 .findFirst();
+    }
+
+    private void ombrelloneNullException(Ombrellone ombrellone) {
+        if (ombrellone == null)
+            throw new NullPointerException("Ombrellone null!");
     }
 }
