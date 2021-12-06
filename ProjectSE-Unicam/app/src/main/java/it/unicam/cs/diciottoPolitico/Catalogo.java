c394dae6f1cfe77f435dfd0df54a7d4385f268f1
HTTPiego <94863664+HTTPiego@users.noreply.github.com>
2021-12-06 15:16:54 +0100

    Create Catalogo.java

---
+++ ProjectSE-Unicam/app/src/main/java/it/unicam/cs/diciottoPolitico/Catalogo.java
@@ -0,0 +1,18 @@
+package it.unicam.cs.diciottoPolitico;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+/**
+ * Interfaccia che descrive un catologo
+ * @param <R> Righe Cataloghi
+ */
+public interface Catalogo<S, R extends InterfaceRigaCatalogo<S>> {
+
+    boolean addRiga(R righa);
+
+    boolean remove(R riga);
+
+    List<R> getRigaBy(Predicate<R> predicate);
+
+}
