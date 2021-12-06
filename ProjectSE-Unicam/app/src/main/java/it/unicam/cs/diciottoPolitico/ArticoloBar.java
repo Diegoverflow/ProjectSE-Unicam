8cac467cab516d334d689ead113162f966235d57
Piero <piero.hierro1@gmail.com>
2021-12-06 18:09:32 +0100

    added api

--- ProjectSE-Unicam/app/src/main/java/it/unicam/cs/diciottoPolitico/ArticoloBar.java
+++ ProjectSE-Unicam/app/src/main/java/it/unicam/cs/diciottoPolitico/ArticoloBar.java
@@ -1,7 +1,28 @@
 package it.unicam.cs.diciottoPolitico;
 
+/**
+ * Rappresenta un articolo bar
+ */
 public interface ArticoloBar {
+
+    /**
+     * Ritorna l'id dell'articolo bar.
+     *
+     * @return id dell'articolo bar
+     */
     long getId();
+
+    /**
+     * Ritorna la descrizione dell'articolo bar.
+     *
+     * @return descrizione dell'articolo bar
+     */
     String getDescrizione();
-    void setDescrizione();
+
+    /**
+     * Imposta una nuova descrizione per l'articolo bar.
+     *
+     * @param descrizione nuova descrizione
+     */
+    void setDescrizione(String descrizione);
 }
--- ProjectSE-Unicam/app/src/main/java/it/unicam/cs/diciottoPolitico/SimpleArticoloBar.java
+++ ProjectSE-Unicam/app/src/main/java/it/unicam/cs/diciottoPolitico/SimpleArticoloBar.java
@@ -1,18 +1,44 @@
 package it.unicam.cs.diciottoPolitico;
 
+/**
+ * Semplice implementazione dell'interfaccia ArticoloBar
+ */
 public class SimpleArticoloBar implements ArticoloBar{
+    private long id;
+    private String descrizione;
+
+    /**
+     * Metodo Costruttore.
+     *
+     * @param id id dell'articolo bar
+     * @param descrizione descrizione dell'articolo bar
+     */
+    public SimpleArticoloBar(long id, String descrizione) {
+        this.id = id;
+        this.descrizione = descrizione;
+    }
+
+    /**
+     * Metodo Costruttore senza id.
+     *
+     * @param descrizione descrizione dell'articolo bar
+     */
+    public SimpleArticoloBar(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
     @Override
     public long getId() {
-        return 0;
+        return this.id;
     }
 
     @Override
     public String getDescrizione() {
-        return null;
+        return this.descrizione;
     }
 
     @Override
-    public void setDescrizione() {
-
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
     }
 }
