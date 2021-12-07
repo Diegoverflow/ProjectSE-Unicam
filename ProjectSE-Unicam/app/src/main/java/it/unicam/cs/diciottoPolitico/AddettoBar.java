bc76db2ffd9d090bf260bcf1946a76366d277f24
Piermuz <piermichele.rosati@gmail.com>
2021-12-07 17:20:52 +0100

    AddettoBar completata

--- ProjectSE-Unicam/app/src/main/java/it/unicam/cs/diciottoPolitico/AddettoBar.java
+++ ProjectSE-Unicam/app/src/main/java/it/unicam/cs/diciottoPolitico/AddettoBar.java
@@ -8,29 +8,100 @@ package it.unicam.cs.diciottoPolitico;
 public interface AddettoBar {
 
     /**
-     * Restituisce true se l' ordinazione relativa all'id specificato viene consegnata
-     * con successo, false altrimenti.
+     * Restituisce {@code true} se l' ordinazione relativa all'id specificato viene consegnata
+     * con successo, {@code false} altrimenti.
      *
      * @param idOrdinazione l'id dell' ordinazione da consegnare
-     * @return true se l' ordinazione viene consegnata con successo, false altrimenti
+     * @return {@code true} se l' ordinazione viene consegnata con successo, {@code false} altrimenti
      */
     boolean consegnaOrdine(long idOrdinazione);
 
     /**
-     * Restituisce true se l' ordinazione relativa all' id specificato viene presa in carico,
-     * false altrimenti.
+     * Restituisce {@code true} se l' ordinazione relativa all' id specificato viene presa in carico,
+     * {@code false} altrimenti.
      *
      * @param idOrdinazione l'id dell' ordinazione da prendere in carico
-     * @return true se l' ordinazione viene presa in carico, false altrimenti
+     * @return {@code true} se l' ordinazione viene presa in carico, {@code false} altrimenti
      */
     boolean prendiInCarico(long idOrdinazione);
 
     /**
      * Una volta che l' ordine &egrave; stato preso in carico, elimina la notifica relativa a quest' ordine.
-     * Restituisce true se la notifica viene eliminata, false alteimenti.
+     * Restituisce {@code true} se la notifica viene eliminata, {@code false} altrimenti.
      *
      * @param idNotifica l'id della notifica da eliminare
-     * @return true se la notifica viene eliminata, false altrimenti
+     * @return {@code true} se la notifica viene eliminata, {@code false} altrimenti
      */
     boolean removeNotifica(long idNotifica);
+
+    /**
+     * Restituisce l' id di questo addetto bar
+     *
+     * @return l' id dell' addetto bar
+     */
+    long getId();
+
+    /**
+     * Restituisce il nome di questo addetto bar.
+     *
+     * @return il nome dell' addetto bar
+     */
+    String getNome();
+
+    /**
+     * Restituisce il cognome di questo addetto bar.
+     *
+     * @return il cognome dell' addetto bar
+     */
+    String getCognome();
+
+    /**
+     * Restituisce la password di questo addetto bar.
+     *
+     * @return la password dell' addetto bar
+     */
+    String getPassword();   // TODO: Ricordarsi di hashare le passsword per non mostrarle in chiaro!
+
+    /**
+     * Imposta il nome specificato all' addetto bar
+     *
+     * @param nome il nome da impostare all' addetto bar
+     */
+    void setNome(String nome);
+
+    /**
+     * Imposta il cognome specificato all' addetto bar
+     *
+     * @param cognome il cognome da impostare all' addetto bar
+     */
+    void setCognome(String cognome);
+
+    /**
+     * Imposta la password specificata all' addetto bar
+     *
+     * @param password la password da impostare all' addetto bar
+     */
+    void setPassword(String password);
+
+
+    /**
+     * Confronta l' elemento specificato con questo addetto bar.
+     * Restituisce {@code true} se l' elemento specificato &egrave; anch' esso un addetto bar ed
+     * ha gli stessi attributi di questo addetto bar.
+     * (Due elementi {@code e1} e {@code e2} sono <i>uguali</i> se {@code Objects.equals(e1,e2)}.)
+     *
+     * @param o l' elemento da essere confrontato per l' uguaglianza con questo addetto bar
+     * @return {@code true} se l' elemento specificato &egrave; uguale a questo addetto bar
+     */
+    boolean equals(Object o);
+
+    /**
+     * Restituisce l' hashCode per questo addetto bar.
+     *
+     * @return l' hashCode per questo addetto bar
+     * @see Object#equals(Object)
+     * @see #equals(Object)
+     */
+    int hashCode();
+
 }
