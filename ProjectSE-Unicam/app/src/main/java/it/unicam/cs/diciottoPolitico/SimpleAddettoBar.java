39997e921b0ae561a29488a0ccea99b1b9baae4e
Piermuz <piermichele.rosati@gmail.com>
2021-12-08 17:00:03 +0100

    Modificato javadoc

--- ProjectSE-Unicam/app/src/main/java/it/unicam/cs/diciottoPolitico/SimpleAddettoBar.java
+++ ProjectSE-Unicam/app/src/main/java/it/unicam/cs/diciottoPolitico/SimpleAddettoBar.java
@@ -16,7 +16,7 @@ public class SimpleAddettoBar implements AddettoBar {
  //tOdO lista di h0rdini consegnati(da questo cliente) hE spostarE metodi sull'andler
     /**
      * @link {https://www.google.com/search?q=uomo+che+dorme&client=firefox-b-d&sxsrf=AOaemvJLUBVPxFt-cgCBWq3HmSg3qPWRjA:1638974026904&tbm=isch&source=iu&ictx=1&fir=i6PNe-OlOFicJM%252CQKxMucXT4LEodM%252C_%253B-Fyx7NfMn7kAxM%252CoXUihJRJYXF3yM%252C_%253B5TN6_r-7tKqV3M%252CKJbnn_aw6OnQUM%252C_%253B_fPD0JYnZC3NKM%252CdyE47U1FzIfPDM%252C_%253BCnwH1azAGgG6aM%252CUVsa0gEpEs9ZbM%252C_%253Bbe2beVQBGoC5FM%252Cy8tp1-H98PqyGM%252C_%253BR0_GZ2jy1Lo8YM%252Cjz68LeW3dUii7M%252C_%253Ba8nCc5o40Xvj-M%252C49BV6u0HAK9akM%252C_%253BhYbwlX83SIVDoM%252C_LPfFM_gYSmMbM%252C_%253Bt-4HAC5GsJsvMM%252CTH40Er5y4jklJM%252C_%253B7qw8MklDnYgHVM%252Clpdd7lj84JVyEM%252C_%253BWt4KabQProJLOM%252C0VXKSqz2YgrU4M%252C_&vet=1&usg=AI4_-kRzsgYs4qYH_k0nBx_NEPJJmtgIlQ&sa=X&ved=2ahUKEwiTh62FttT0AhURCewKHbn_AuoQ9QF6BAgPEAE&biw=1536&bih=739&dpr=1.25#imgrc=hYbwlX83SIVDoM}
-     * Crea un semplice addetto bar in base all' id, nome, cognome e password specificsati.
+     * Crea un semplice addetto bar in base all' id, nome, cognome e password specificati.
      *
      * @param id                    l' id dell' addetto bar da creare
      * @param nome                  il nome dell' addetto bar da creare
