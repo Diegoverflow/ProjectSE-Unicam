97d17f95bbf4ca036ba80489197286413fdb71a9
Piero <piero.hierro1@gmail.com>
2021-12-09 12:12:45 +0100

    implemented RigaCatalogoOmbrellone

--- ProjectSE-Unicam/app/src/main/java/it/unicam/cs/diciottoPolitico/RigaCatalogoOmbrellone.java
+++ ProjectSE-Unicam/app/src/main/java/it/unicam/cs/diciottoPolitico/RigaCatalogoOmbrellone.java
@@ -1,13 +1,14 @@
 package it.unicam.cs.diciottoPolitico;
 
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 /**
  * Rappresenta una riga di un catalogo ombrelloni.
  * La riga &egrave; una tripla formata da Ombrellone, prezzo e la lista delle prenotazioni dell' ombrellone.
  */
-public interface RigaCatalogoOmbrellone {
+public interface RigaCatalogoOmbrellone extends RigaCatalogo<Ombrellone>{
 
     /**
      * Restituisce il prezzo associato all' ombrellone presente in questa riga.
@@ -15,13 +16,6 @@ public interface RigaCatalogoOmbrellone {
      * @return il prezzo associato all' ombrellone
      */
     double getPrezzoOmbrellone();
-
-    /**
-     * Restituisce l' ombrellone presente in questa riga.
-     *
-     * @return l' ombrellone di questa riga
-     */
-    Ombrellone getOmbrellone();
 
     /**
      * Restituisce la lista delle prenotazioni associate all' ombrellone di questa riga.
@@ -34,18 +28,18 @@ public interface RigaCatalogoOmbrellone {
      * Determina se l' ombrellone di questa riga &egrave; disponibile in base alla data e la fascia oraria specificate.
      * Restituisce true se c'&egrave; disponibilit&agrave;, false altrimenti.
      *
-     * @param date         la data in cui verificare se l' ombrellone &egrave; disoonibile oppure no.
+     * @param data         la data in cui verificare se l' ombrellone &egrave; disoonibile oppure no.
      * @param fasciaOraria la fascia oraria in cui verificare se l' ombrellone &egrave; disoonibile oppure no.
      * @return true se l' ombrellone &egrave; disponibile per la data e la fascia oraria specificate, false altrimenti.
      * @throws NullPointerException se almeno uno dei parametri specificati &egrave; null
      */
-    boolean getDisponibilita(Date date, FasciaOraria fasciaOraria);
+    boolean getDisponibilita(GregorianCalendar data, FasciaOraria fasciaOraria);
 
     /**
      * Imposta un nuovo prezzo all' ombrellone di questa riga.
      *
      * @param nuovoPrezzo il nuovo prezzo da impostare all' ombrellone di questa riga.
-     * @throws IllegalArgumentException se il prezzo specificato non &egrave; valido
+     * @throws IllegalArgumentException se il prezzo specificato &egrave; minore di 0
      */
     void setPrezzoOmbrellone(double nuovoPrezzo);
 
