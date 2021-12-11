d3f06912059cfe6a02924f93d9ef142bd761f4e1
Piermuz <piermichele.rosati@gmail.com>
2021-12-11 11:19:37 +0100

    Cambiato nome da ordinazioneBarConsegnate a ordinazioniBarConsegnate

--- ProjectSE-Unicam/app/src/main/java/it/unicam/cs/diciottoPolitico/SimpleAddettoBar.java
+++ ProjectSE-Unicam/app/src/main/java/it/unicam/cs/diciottoPolitico/SimpleAddettoBar.java
@@ -15,27 +15,26 @@ public class SimpleAddettoBar implements AddettoBar {
     private String password;
     private String numero;
     private final Queue<Notifica> notifiche;
-    private final Queue<OrdinazioneBar> ordinazioneBarConsegnate;
+    private final Queue<OrdinazioneBar> ordinazioniBarConsegnate;
 
     /**
      * Crea un semplice addetto bar in base all' id, nome, cognome e password specificati.
      *
-     * @param id                    l' id dell' addetto bar da creare
-     * @param nome                  il nome dell' addetto bar da creare
-     * @param cognome               il cognome dell' addetto bar da creare
-     * @param password              la password dell' addetto bar da creare
-     * @param handlerOrdinazioneBar l' handler per le ordinazioni bar con cui l' addetto interagisce
+     * @param id       l' id dell' addetto bar da creare
+     * @param nome     il nome dell' addetto bar da creare
+     * @param cognome  il cognome dell' addetto bar da creare
+     * @param password la password dell' addetto bar da creare
      * @throws NullPointerException se uno dei parametri &egrave; nullo
      */
 
-    public SimpleAddettoBar(long id, String nome, String numero, String cognome, String password, HandlerOrdinazioneBar handlerOrdinazioneBar) {
+    public SimpleAddettoBar(long id, String nome, String numero, String cognome, String password) {
         this.id = id;
         this.nome = Objects.requireNonNull(nome, "Nome null!");
         this.cognome = Objects.requireNonNull(cognome, "Cognome null!");
         this.password = Objects.requireNonNull(password, "Password null!");
         this.numero = Objects.requireNonNull(numero, "Il numero non puo' essere nullo");
         this.notifiche = new LinkedList<>();
-        this.ordinazioneBarConsegnate = new LinkedList<>();
+        this.ordinazioniBarConsegnate = new LinkedList<>();
     }
 
     @Override
@@ -102,8 +101,8 @@ public class SimpleAddettoBar implements AddettoBar {
 
 
     @Override
-    public Queue<OrdinazioneBar> getOrdinazione() {
-        return this.ordinazioneBarConsegnate;
+    public Queue<OrdinazioneBar> getordinazioniBarConsegnate() {
+        return this.ordinazioniBarConsegnate;
     }
 
     @Override
