6698aa63873860059d49a68282febc84fc3575f1
Piermuz <piermichele.rosati@gmail.com>
2021-12-16 17:35:09 +0100

    Aggiornato javadoc

--- ProjectSE-Unicam/app/src/main/java/it/unicam/cs/diciottoPolitico/Categoria.java
+++ ProjectSE-Unicam/app/src/main/java/it/unicam/cs/diciottoPolitico/Categoria.java
@@ -1,7 +1,17 @@
 package it.unicam.cs.diciottoPolitico;
 
 /**
- * Tipologie di Categoria per gli ombrelloni.
+ * Un ombrellone &egrave; di una categoria tra le seguenti:
+ * <ul>
+ *  <li>{@link #STANDARD}<br>
+ *        Un ombrellone economico.
+ *  </li>
+ *  <li>{@link #VIP}<br>
+ *        Un ombrellone //TODO: completare
+ *  </li>
+ *  <li>{@link #PREMIUM}<br>
+ *        Un ombrellone //TODO: completare
+ *  </ul>
  */
 public enum Categoria {
     STANDARD, VIP, PREMIUM
