3202606b1e448905b33e2060fb75db6f2113ba98
Piermuz <piermichele.rosati@gmail.com>
2021-12-23 11:24:13 +0100

    Created TipologiaUtente.java

---
+++ ProjectSE-Unicam/app/src/main/java/it/unicam/cs/diciottoPolitico/TipologiaUtente.java
@@ -0,0 +1,23 @@
+package it.unicam.cs.diciottoPolitico;
+
+/**
+ * Una tipologia a cui un utente che ha effettuato l' autenticazione, ovvero un {@link UtenteLoggato} appartiene.
+ * Un {@link UtenteLoggato} pu&ograve; essere appartenerein una delle seguenti tipologie:
+ *  * <ul>
+ *  *      <li>{@link #GESTORE}<br>
+ *  *      Un utente che viene considerato a tutti gli effetti un gestore.
+ *  *           </li>
+ *  *      <li>{@link #CASSIERE}<br>
+ *  *      Un utente che &egrave; stato abilitato per essere un cassiere.
+ *  *           </li>
+ *  *       <li>{@link #ADDETTO_BAR}<br>
+ *  *            Un utente che &egrave; stato abilitato per essere un addetto bar.
+ *  *       </ul>
+ *  *
+ *  * <p>
+ *  *     Un utente pu&ograve; appartenere ad una sola tipologia alla volta // TODO: una o più tipologie?
+ *  * </p>
+ */
+public enum TipologiaUtente {
+    GESTORE, CASSIERE, ADDETTO_BAR
+}
