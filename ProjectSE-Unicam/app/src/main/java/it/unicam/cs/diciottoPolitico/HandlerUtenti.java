0dadeee9e0ab6e8621bd2a658fab152f117b7fc4
HTTPiego <belardinelli.diego@gmail.com>
2022-01-15 20:14:14 +0100

    Definita interfaccia HandlerUtenti

--- ProjectSE-Unicam/app/src/main/java/it/unicam/cs/diciottoPolitico/HandlerUtenti.java
+++ ProjectSE-Unicam/app/src/main/java/it/unicam/cs/diciottoPolitico/HandlerUtenti.java
@@ -1,5 +1,47 @@
 package it.unicam.cs.diciottoPolitico;
 
+import java.util.List;
+
 public interface HandlerUtenti {
+
+    // TODO: 15/01/22   il vpp ha le liste parametrizzate con CLIENTE CASSIERE ETC
+            
+
+    List getClienti();
+
+    List getPersonaleBar();
+
+    List getCassieri();
+
+    List getGestori();
+
+    boolean creaCliente( String nome,
+                         String cognome,
+                         String password,
+                         String cellulare,
+                         String email);
+
+    boolean creaPersonaleBar( String nome,
+                         String cognome,
+                         String password,
+                         String cellulare,
+                         String email);
+
+    boolean creaCassiere( String nome,
+                         String cognome,
+                         String password,
+                         String cellulare,
+                         String email);
+
+    boolean creaGestore( String nome,
+                         String cognome,
+                         String password,
+                         String cellulare,
+                         String email);
+
+    boolean eliminaUtente (long codice);
+
+    boolean autenticarsi (String email, String password);
+    
 
 }
---
+++ ProjectSE-Unicam/app/src/main/java/it/unicam/cs/diciottoPolitico/SimpleHandleUtenti.java
@@ -0,0 +1,4 @@
+package it.unicam.cs.diciottoPolitico;
+
+public class SimpleHandleUtenti {
+}
