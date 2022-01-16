162e5e0f56b0f9cbad9bd58b6459210c1365c8ad
HTTPiego <belardinelli.diego@gmail.com>
2022-01-16 02:03:03 +0100

    aggiornato JavaDoc HandlerCatalogo

--- ProjectSE-Unicam/app/src/main/java/it/unicam/cs/diciottoPolitico/HandlerCatalogo.java
+++ ProjectSE-Unicam/app/src/main/java/it/unicam/cs/diciottoPolitico/HandlerCatalogo.java
@@ -8,7 +8,7 @@ public interface HandlerCatalogo <R> {
 
     /**
      * Aggiunge una riga al catalogo
-     * @param rigaDaAggiungere riga da aggiungere
+     * @param rigaDaAggiungere .
      * @return {@code true} se la riga viene aggiunta
      *         {@code false} altrimenti
      */
@@ -16,12 +16,19 @@ public interface HandlerCatalogo <R> {
 
     /**
      * Rimuovi una riga dal catalogo
-     * @param rigaDaEliminare riga da elimininare
+     * @param rigaDaEliminare .
      * @return {@code true} se la riga &grave; stata rimossa
      *         {@code false} altrimenti
      */
     boolean rimuoviRigaCatalogo(R rigaDaEliminare);
 
+    /**
+     * Modifica il prezzo di una certa riga presente sul catalogo
+     * @param rigaCatalogo di cui si vuole aggiornare il prezzo
+     * @param nuovoPrezzo da impostare
+     * @return {@code true} se il prezzo della riga &grave; stato aggiornato
+     *         {@code false} altrimenti
+     */
     boolean modificaPrezzoRigaCatalogo(R rigaCatalogo, double nuovoPrezzo);
 
 }
