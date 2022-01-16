7f47af9410edf07bf4489ee2d88135514ffbb7d5
HTTPiego <belardinelli.diego@gmail.com>
2022-01-16 03:20:13 +0100

    aggiornato SimpleHandlerCatalogoOmbrelloni

--- ProjectSE-Unicam/app/src/main/java/it/unicam/cs/diciottoPolitico/SimpleHandlerCatalogoOmbrelloni.java
+++ ProjectSE-Unicam/app/src/main/java/it/unicam/cs/diciottoPolitico/SimpleHandlerCatalogoOmbrelloni.java
@@ -1,4 +1,22 @@
 package it.unicam.cs.diciottoPolitico;
 
-public class SimpleHandlerCatalogoOmbrelloni {
+public class SimpleHandlerCatalogoOmbrelloni implements HandlerCatalogo<RigaCatalogoOmbrellone>{
+
+    Catalogo<Ombrellone, RigaCatalogoOmbrellone> rigaCatalogoOmbrellone;
+
+
+    @Override
+    public boolean aggiungiRigaCatalogo(RigaCatalogoOmbrellone rigaDaAggiungere) {
+        return false;
+    }
+
+    @Override
+    public boolean rimuoviRigaCatalogo(RigaCatalogoOmbrellone rigaDaEliminare) {
+        return false;
+    }
+
+    @Override
+    public void modificaPrezzoRigaCatalogo(RigaCatalogoOmbrellone rigaCatalogo, double nuovoPrezzo) {
+
+    }
 }
