a2fbb5b1709b5ee84dd1e22a7fd1d9b92c965c74
Piero <68665714+xPieroxk@users.noreply.github.com>
2022-01-20 15:19:41 +0100

    Delete ProjectSE-Unicam directory

--- ProjectSE-Unicam/app/src/main/java/it/unicam/cs/diciottoPolitico/StatusOrdinazioneBar.java
+++
@@ -1,5 +0,0 @@
-package it.unicam.cs.diciottoPolitico;
-
-public enum StatusOrdinazioneBar {
-    DA_PRENDERE_IN_CARICO, PRESO_IN_CARICO, CONSEGNATO
-}
--- ProjectSE-Unicam/app/src/main/java/it/unicam/cs/diciottoPolitico/TipologieCatalogo.java
+++
@@ -1,7 +0,0 @@
-package it.unicam.cs.diciottoPolitico;
-
-public enum TipologieCatalogo {
-    BAR,
-    OMBRELLONI,
-    ATTIVITA
-}
