05d385efe6a3f7bbe9e93ad5403998a60c75e9d7
HTTPiego <belardinelli.diego@gmail.com>
2022-01-24 00:46:34 +0100

    PrenotazioneOmbrelloneService

---
+++ src/main/java/it/unicam/cs/diciottoPolitico/casotto/service/PrenotazioneOmbrelloneService.java
@@ -0,0 +1,16 @@
+package it.unicam.cs.diciottoPolitico.casotto.service;
+
+import it.unicam.cs.diciottoPolitico.casotto.repository.PrenotazioneOmbrelloneRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PrenotazioneOmbrelloneService {
+
+    private final PrenotazioneOmbrelloneRepository repository;
+
+    @Autowired
+    public PrenotazioneOmbrelloneService(PrenotazioneOmbrelloneRepository repository) {
+        this.repository = repository;
+    }
+}
