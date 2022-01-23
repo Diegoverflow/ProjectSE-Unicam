dceeaa99828bc924f0fb50e50eb6050247fd81ad
HTTPiego <belardinelli.diego@gmail.com>
2022-01-24 00:46:06 +0100

    PrenotazioneAttivitaService

---
+++ src/main/java/it/unicam/cs/diciottoPolitico/casotto/service/PrenotazioneAttivitaService.java
@@ -0,0 +1,16 @@
+package it.unicam.cs.diciottoPolitico.casotto.service;
+
+import it.unicam.cs.diciottoPolitico.casotto.repository.PrenotazioneAttivitaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PrenotazioneAttivitaService {
+
+    private final PrenotazioneAttivitaRepository repository;
+
+    @Autowired
+    public PrenotazioneAttivitaService(PrenotazioneAttivitaRepository repository) {
+        this.repository = repository;
+    }
+}
