389bf184d737b63e19116a2b2271af41fa0a25a2
Piermuz <piermichele.rosati@gmail.com>
2022-01-25 15:52:59 +0100

    Added extends AbstractServie

--- src/main/java/it/unicam/cs/diciottoPolitico/casotto/service/InfrastrutturaService.java
+++ src/main/java/it/unicam/cs/diciottoPolitico/casotto/service/InfrastrutturaService.java
@@ -5,9 +5,7 @@ import it.unicam.cs.diciottoPolitico.casotto.repository.InfrastrutturaRepository
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 /**
  * Service dell' infrastruttura dello chalet.
@@ -18,18 +16,11 @@ import java.util.UUID;
  * @see InfrastrutturaRepository
  */
 @Service
-public class InfrastrutturaService {
+public class InfrastrutturaService extends AbstractService<AreaInfrastruttura, InfrastrutturaRepository> {
 
     @Autowired
-    private InfrastrutturaRepository infrastrutturaRepository;
-
-    /**
-     * Restituisce la lista di tutte le aree presenti nel database.
-     *
-     * @return la lista di tutte le aree presenti nel database
-     */
-    public List<AreaInfrastruttura> getAllAree() {
-        return this.infrastrutturaRepository.findAll();
+    public InfrastrutturaService(InfrastrutturaRepository repository) {
+        super(repository);
     }
 
     /**
@@ -41,55 +32,12 @@ public class InfrastrutturaService {
      * oppure un empty {@code Optional} se non viene trovata nessun' area con il nome specificato
      */
     public Optional<AreaInfrastruttura> getAreaByName(String nome) {
-        return this.infrastrutturaRepository.findAll()
+        // TODO: completare
+        /*return this.infrastrutturaRepository.findAll()
                 .stream()
                 .filter(a -> a.getNome().equals(nome))
-                .findFirst();
-    }
-
-    /**
-     * Aggiunge l' {@link AreaInfrastruttura} specificata al database.
-     * Restituisce un {@link Optional} che descrive l' {@code AreaInfrastruttura}
-     * oppure un empty {@code Optional} se l' area &egrave; gi&agrave; presente nel database.
-     *
-     * @param areaInfrastruttura l' area da aggiungere al database
-     * @return un {@code Optional} che descrive l' {@code AreaInfrastruttura} da aggiungere al database
-     * oppure un empty {@code Optional} se l' area &egrave; gi&agrave; presente nel database
-     */
-    public Optional<AreaInfrastruttura> addArea(AreaInfrastruttura areaInfrastruttura) {
-        if (this.infrastrutturaRepository.findAll().stream().noneMatch(a -> a.equals(areaInfrastruttura)))
-            return Optional.of(this.infrastrutturaRepository.save(areaInfrastruttura));
+                .findFirst();*/
         return Optional.empty();
-    }
-
-    /**
-     * Aggiorna l' {@link AreaInfrastruttura} specificata nel database.
-     * Restituisce un {@link Optional} che descrive l' {@code AreaInfrastruttura} aggiornata nel database
-     * oppure un empty {@code Optional} se l' area non &egrave; presente nel database.
-     *
-     * @param areaInfrastruttura l'{@code AreaInfrastruttura} da aggiornare dal database
-     * @return un {@code Optional} che descrive l' {@code AreaInfrastruttura} aggiornata nel database
-     * oppure un empty {@code Optional} se l' area non &egrave; presente nel database
-     */
-    public Optional<AreaInfrastruttura> updateArea(AreaInfrastruttura areaInfrastruttura) {
-        if (this.infrastrutturaRepository.findById(areaInfrastruttura.getId()).isPresent())
-            return Optional.of(this.infrastrutturaRepository.save(areaInfrastruttura));
-        return Optional.empty();
-    }
-
-    /**
-     * Rimuove l' {@link AreaInfrastruttura} avente id specificato dal database.
-     * Restituisce un {@link Optional} che descrive l' {@code AreaInfrastruttura} rimossa dal database
-     * oppure un empty {@code Optional} se l' area non &egrave; presente nel database.
-     *
-     * @param id l' id dell' {@code AreaInfrastruttura} da rimuovere dal database
-     * @return un {@code Optional} che descrive l' {@code AreaInfrastruttura} rimossa dal database
-     * oppure un empty {@code Optional} se l' area non &egrave; presente nel database
-     */
-    public Optional<AreaInfrastruttura> removeAreaBy(UUID id) {
-        Optional<AreaInfrastruttura> foundArea = this.infrastrutturaRepository.findById(id);
-        foundArea.ifPresent(areaInfrastruttura -> this.infrastrutturaRepository.delete(areaInfrastruttura));
-        return foundArea;
     }
 
 }
--- src/main/java/it/unicam/cs/diciottoPolitico/casotto/service/RigaCatalogoBarService.java
+++ src/main/java/it/unicam/cs/diciottoPolitico/casotto/service/RigaCatalogoBarService.java
@@ -6,33 +6,12 @@ import it.unicam.cs.diciottoPolitico.casotto.repository.RigaCatalogoBarRepositor
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-import java.util.UUID;
-
 @Service
 public class RigaCatalogoBarService extends AbstractService<SimpleRigaCatalogoBar, RigaCatalogoBarRepository> {
 
     @Autowired
     public RigaCatalogoBarService(RigaCatalogoBarRepository repository) {
         super(repository);
-    }
-
-    /**
-     * Aggiunge la {@link SimpleOrdinazioneBar} specificata al database.
-     * Restituisce un {@link Optional} che descrive la {@code SimpleOrdinazioneBar}
-     * oppure un empty {@code Optional} se l' ordinazione &egrave; gi&agrave; presente nel database.
-     *
-     * @param ordinazioneBar l' ordinazione da aggiungere al database
-     * @return un {@code Optional} che descrive l' {@code SimpleOrdinazioneBar} da aggiungere al database
-     * oppure un empty {@code Optional} se l' ordinazione &egrave; gi&agrave; presente nel database
-     */
-    public Optional<SimpleRigaCatalogoBar> addOrdinazione(UUID id, SimpleOrdinazioneBar ordinazioneBar) {
-        Optional<SimpleRigaCatalogoBar> foundRiga = super.repository.findAll().stream().filter(r -> r.getId().equals(id)).findFirst();
-        if (foundRiga.isPresent()) {
-            foundRiga.get().getOrdinazioniBar().add(ordinazioneBar);
-            return Optional.of(super.repository.save(foundRiga.get()));
-        }
-        return Optional.empty();
     }
 
 }
