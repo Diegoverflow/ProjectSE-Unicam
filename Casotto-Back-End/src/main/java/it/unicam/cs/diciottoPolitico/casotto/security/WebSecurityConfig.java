70c8c0426e50be486fea022016df3acbb149a622
Piero <68665714+xPieroxk@users.noreply.github.com>
2022-03-09 10:55:20 +0100

    open POST /utenti endpoint to all users

--- Casotto-Back-End/src/main/java/it/unicam/cs/diciottoPolitico/casotto/security/WebSecurityConfig.java
+++ Casotto-Back-End/src/main/java/it/unicam/cs/diciottoPolitico/casotto/security/WebSecurityConfig.java
@@ -83,46 +83,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        var cookieRepository = CookieCsrfTokenRepository.withHttpOnlyFalse();
-        cookieRepository.setSecure(true);
         http
-                .csrf().csrfTokenRepository(cookieRepository)
-                .and()
-                .cors()
-                .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .addFilter(new JwtAuthenticationFilter(authenticationManager(), this.jwtConfig))
-                .addFilterAfter(new JwrAuthorizationFilter(this.jwtConfig), JwtAuthenticationFilter.class)
-                .addFilterBefore(new GlobalExceptionHandlerFilter(this.handlerExceptionResolver),JwtAuthenticationFilter.class)
-                .authorizeRequests()
-                .expressionHandler(webExpressionHandler())
-                .antMatchers("/infrastruttura/aree/**").hasRole("GESTORE")
-                .antMatchers(HttpMethod.POST, "/bar/ordinazioni").hasRole("CLIENTE")
-                .antMatchers(HttpMethod.GET, "/bar/ordinazioni/disponibili").hasRole("CLIENTE")
-                .antMatchers("/bar/ordinazioni/**").hasAnyRole("ADDETTO_BAR", "CASSIERE")
-                .antMatchers(HttpMethod.GET, "/prenotazioni/attivita/disponibili").hasRole("CLIENTE")
-                .antMatchers(HttpMethod.POST, "/prenotazioni/attivita").hasRole("CLIENTE")
-                .antMatchers("/prenotazioni/attivita/**").hasRole("GESTORE")
-                .antMatchers(HttpMethod.GET, "/prenotazioni/ombrelloni/disponibili/{data}/{fasciaOraria}").hasRole("CLIENTE")
-                .antMatchers(HttpMethod.POST, "/prenotazioni/ombrelloni").hasRole("CLIENTE")
-                .antMatchers("/prenotazioni/ombrelloni/**").hasRole("GESTORE")
-                .antMatchers("/catalogo/**").hasRole("GESTORE")
-                .antMatchers(HttpMethod.POST, "/utenti").permitAll()
-                .antMatchers("/utenti/ruolo").permitAll()
-                .antMatchers(HttpMethod.POST,"/utenti").permitAll()
-                .antMatchers("/utenti/**").hasRole("GESTORE")
-                .antMatchers("/vendite/utente/{idUtente}/da-pagare").hasAnyRole("CLIENTE", "CASSIERE")
-                .antMatchers("/vendite/utente/{idUtente}/all").hasAnyRole("CLIENTE", "CASSIERE")
-                .antMatchers(HttpMethod.PATCH, "/vendite/{idVendita}/{isPagato}").hasRole("CASSIERE")
-                .antMatchers("/vendite/all").hasRole("CASSIERE")
-                .antMatchers("/vendite/**").hasRole("GESTORE")
-                .antMatchers("/login","/check-token").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .logout()
-                .logoutSuccessHandler((httpServletRequest, httpServletResponse, authentication) -> httpServletResponse.setStatus(HttpServletResponse.SC_OK))
-                .deleteCookies(this.jwtConfig.getCookieName());
+                .csrf().disable();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/**");
     }
 
 }
