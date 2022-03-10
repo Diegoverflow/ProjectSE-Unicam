3232b3ff7d7f858d067ce19daf78daf785d9024f
HTTPiego <belardinelli.diego@gmail.com>
2022-03-10 10:10:28 +0100

    primary

--- Casotto-Back-End/src/main/java/it/unicam/cs/diciottoPolitico/casotto/security/WebSecurityConfig.java
+++ Casotto-Back-End/src/main/java/it/unicam/cs/diciottoPolitico/casotto/security/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import it.unicam.cs.diciottoPolitico.casotto.security.jwt.JwtAuthenticationFilte
 import it.unicam.cs.diciottoPolitico.casotto.security.jwt.JwtConfig;
 import it.unicam.cs.diciottoPolitico.casotto.utils.exception.GlobalExceptionHandlerFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
