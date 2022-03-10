c4d3822a4663dc76fc218ca4ce50600a281e7a2d
HTTPiego <belardinelli.diego@gmail.com>
2022-03-10 10:13:17 +0100

    primary tag

--- Casotto-Back-End/src/main/java/it/unicam/cs/diciottoPolitico/casotto/security/MyUserDetailService.java
+++ Casotto-Back-End/src/main/java/it/unicam/cs/diciottoPolitico/casotto/security/MyUserDetailService.java
@@ -3,12 +3,14 @@ package it.unicam.cs.diciottoPolitico.casotto.security;
 import it.unicam.cs.diciottoPolitico.casotto.model.SimpleUtente;
 import it.unicam.cs.diciottoPolitico.casotto.repository.UtenteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@Primary
 public class MyUserDetailService implements UserDetailsService {
 
     @Autowired
