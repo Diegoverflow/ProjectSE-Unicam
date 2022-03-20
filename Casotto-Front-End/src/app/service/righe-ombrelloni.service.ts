2e0cc3d99711a2c922b5f0440477bc9cb89ec344
Piermuz7 <piermichele.rosati@gmail.com>
2022-03-21 00:33:27 +0100

    fixed ordinazione bar and righe ombrelloni service

--- Casotto-Front-End/src/app/service/righe-ombrelloni.service.ts
+++ Casotto-Front-End/src/app/service/righe-ombrelloni.service.ts
@@ -9,7 +9,7 @@ import { environment } from 'src/environments/environment';
 })
 export class RigheOmbrelloniService {
 
-  private apiServerUrl: string = environment.apiBaseUrl + + '/catalogo/ombrelloni';
+  private apiServerUrl: string = environment.apiBaseUrl + '/catalogo/ombrelloni';
 
   constructor(private http: HttpClient) { }
 
